c2d6254dacc38c7c938ed46d5b42ee777c6861cd
alyssa horrocks & evan clough <student@epicodus.com>
2015-08-17 14:49:49 -0700

    fix due_date not equally thingy

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,4 +1,5 @@
 require('spec_helper')
+require('pry')
 
 describe(Task) do
   describe(".all") do
@@ -9,30 +10,37 @@ describe(Task) do
 
   describe("#save") do
     it("adds a task to the array of saved tasks") do
-      test_task = Task.new({:description => 'learn SQL', :list_id => 1})
+      test_task = Task.new({:description => 'learn SQL', :due_date => '2015-08-24 00:00:00', :list_id => 1})
       test_task.save()
-      expect(Task.all()).to(eq([test_task]))
+      expect(Task.all.first).to(eq(test_task))
     end
   end
 
   describe("#description") do
     it('lets you read the description out') do
-        test_task = Task.new({:description => 'eat tacos', :list_id => 1})
+        test_task = Task.new({:description => 'eat tacos', :due_date => '2015-08-24 00:00:00', :list_id => 1})
         expect(test_task.description()).to(eq('eat tacos'))
     end
   end
 
   describe('#list_id') do
     it('lets you read the list ID out') do
-      test_task = Task.new({:description => 'eat tacos', :list_id => 1})
+      test_task = Task.new({:description => 'eat tacos', :due_date => '2015-08-24 00:00:00', :list_id => 1})
       expect(test_task.list_id()).to(eq(1))
+    end
+  end
+
+  describe('#due_date') do
+    it('returns the due date of the task') do
+      test_task = Task.new({:description => 'eat tacos', :due_date => '2015-08-24 00:00:00', :list_id => 1})
+      expect(test_task.due_date()).to(eq('2015-08-24 00:00:00'))
     end
   end
 
   describe('#==') do
     it('is the same task if it has the same description') do
-      task1 = Task.new({:description => 'learn SQL', :list_id => 1})
-      task2 = Task.new({:description => 'learn SQL', :list_id => 1})
+      task1 = Task.new({:description => 'learn SQL', :due_date => '2015-08-24 00:00:00', :list_id => 1})
+      task2 = Task.new({:description => 'learn SQL', :due_date => '2015-08-24 00:00:00', :list_id => 1})
       expect(task1).to(eq(task2))
     end
   end
